b50838b06cd4177658cc07d43fd3b13580ec7f38
Jens Hiller <jens.hiller@rwth-aachen.de>
2020-05-13 10:46:11 +0200

    Fix data merge for 'all' domains
      Alexa domains are included in other domain lists. Do not count them twice.

--- active-scans-drafts/03_plotting/01_merge_zones.sh
+++ active-scans-drafts/03_plotting/01_merge_zones.sh
@@ -4,5 +4,5 @@ python3 merge.py --prefix=data/ --out _com-net-org --in com-A-www --in net-A-www
 
 python3 merge.py --prefix=data/ --out _all-but-com-net-org $(ls data/ | grep -v -e '^_' -e "^alexa-A-www" -e "^com-A-www" -e "^net-A-www" -e "^org-A-www" | xargs printf -- '--in %s\n' | xargs)
 
-python3 merge.py --prefix=data/ --out _all $(ls data/ | grep -v -e '^_' | xargs printf -- '--in %s\n' | xargs)
+python3 merge.py --prefix=data/ --out _all $(ls data/ | grep -v -e '^_' -e "^alexa-A-www" | xargs printf -- '--in %s\n' | xargs)
 
