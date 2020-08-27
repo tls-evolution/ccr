258d8ce76a4ed668b5a98520e983e54bfa450c94
Jens Hiller <jens.hiller@rwth-aachen.de>
2020-08-27 09:14:01 +0200

    Draft scans: Provide proper example input

--- active-scans-drafts/01_scanner/04_runner-scripts/zgrab_rabbitQueue_tls-13-all-recent_run.sh
+++ active-scans-drafts/01_scanner/04_runner-scripts/zgrab_rabbitQueue_tls-13-all-recent_run.sh
@@ -9,5 +9,5 @@ BLACKLIST_IP=blacklist-ip_example.txt
 BLACKLIST_DOM=blacklist-dom_example.txt
 
 # ulimit -n 900000
-cat example_in.txt | ${ZGRAB} -http-user-agent "${USERAGENT}" --tls --http-max-redirects 20 -http-method GET -http-max-size 0 -port 443 --http="/" -tls-version=TLSv1.3 --blacklist-ip=${BLACKLIST_IP} --blacklist-dom=${BLACKLIST_DOM} -senders 3000 > example_out.json
+cat example_in.txt | HOME="/" jq --unbuffered -c -r 'if .status == "NOERROR" then . as $full | ."comsys-source" as $cs | ."comsys-date" as $cd | .altered_name as $an | [.data.answers[] | select(.type=="A")][0] | if (.answer != null) then [.answer, $an, $cs, $cd, ($full | tostring | [.] | @csv)] | join(",") else empty end else empty end' | ${ZGRAB} -http-user-agent "${USERAGENT}" --tls --http-max-redirects 20 -http-method GET -http-max-size 0 -port 443 --http="/" -tls-version=TLSv1.3 --blacklist-ip=${BLACKLIST_IP} --blacklist-dom=${BLACKLIST_DOM} -senders 3000 > example_out.json
 
